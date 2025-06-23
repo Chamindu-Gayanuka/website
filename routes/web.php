8a525781464a25ee07c60455c87a6f9549291fad
Chamindu Gayanuka <chamindugayanuka2002@gmail.com>
2025-06-23 16:40:48 +0530

    Add test page link to home and create company and organization views

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,30 @@ Route::get('/', function () {
     return view('home');
 });
 
+/*
 // Parameters using routes
 Route::get('/portfolio/{firstname}/{lastname}', function ($firstname, $lastname) {
     return $firstname . " " . $lastname;
 });
 
+
 // Named routes
 Route::get('/test', function (){
     return "This is a test!";
+}) -> name("testpage");
+*/
+
+// Portfolio related routes
+Route::get('/portfolio', function () {
+    return view('portfolio');
+});
+
+Route::prefix("portfolio") -> group(function(){
+    Route::get('/company', function () {
+    return view('company');
+    });
+
+    Route::get('/organization', function () {
+        return view('organization');
+    });
 });
