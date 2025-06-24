bc9b72716d5f1bcc055f3945c2338792be21cce3
Chamindu Gayanuka <chamindugayanuka2002@gmail.com>
2025-06-24 07:35:31 +0530

    Add form to home page and handle submission in web routes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,19 @@
 </head>
 <body>
     <h1>Home</h1>
-    <a href="{{ route("testpage") }}">Go to Test!</a>
+    <form action="{{ url('/formSubmitted') }}" method="post">
+        @csrf
+        <label for="fullName">Full Name: </label>
+        <input type="text" id="fullName" name="fullName" placeholder="Enter Your Full Name" required>
+
+        <br><br>
+
+        <label for="email">Email: </label>
+        <input type="text" id="email" name="email" placeholder="Enter Your Email" required>
+
+        <br><br>
+
+        <button type="submit">Submit</button>
+    </form>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 // Get route example
 Route::get('/', function () {
     return view('home');
 });
 
-/*
-// Parameters using routes
-Route::get('/portfolio/{firstname}/{lastname}', function ($firstname, $lastname) {
-    return $firstname . " " . $lastname;
-});
+// POST route example
+Route::post("/formSubmitted", function (Request $request) {
+    $request -> validate([
+        'fullName' => 'required|min:3|max:30',
+        'email' => 'required|min:3|max30|email',
+    ]);
 
+    $fullName = $request -> input('fullName');
+    $email = $request -> input('email');
 
-// Named routes
-Route::get('/test', function (){
-    return "This is a test!";
-}) -> name("testpage");
-*/
-
-// Portfolio related routes
-Route::get('/portfolio', function () {
-    return view('portfolio');
-});
-
-Route::prefix("portfolio") -> group(function(){
-    Route::get('/company', function () {
-    return view('company');
-    });
-
-    Route::get('/organization', function () {
-        return view('organization');
-    });
-});
+    return "Your full name is: $fullName and your email is: $email";
+})->name("formSubmitted");
